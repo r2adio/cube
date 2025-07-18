eda921b967028f0ca1b5dc38d383e24e934675a8
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-07-18 23:53:47 +0530

    setup trpc and using client component to fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 const Page = () => {
-  return <div>asdfhjkl</div>;
+  const trpc = useTRPC();
+  // trpc.hello.queryOptions({ text: "Hello!" }); //check src/trpc/routers/_app.ts
+  // const { data } = useQuery(fetch("/api/create-ai", { body: JSON.stringify })); //earlier meathod to get data from api
+  // simple fetching data using a client component, this is a data access layer
+  const { data } = useQuery(trpc.createAI.queryOptions({ text: "Antonio" }));
+
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 export default Page;
