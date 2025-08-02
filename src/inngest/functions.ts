c84a86a9a78a7af91aae7e45c7c884b98ee7fb5c
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-08-02 19:56:10 +0530

    feat: add tools to agent - "terminal" - "createOrUpdateFiles" - "readFiles"

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,10 @@
 import Sandbox from "@e2b/code-interpreter";
-import { gemini, createAgent } from "@inngest/agent-kit";
+import { gemini, createAgent, createTool } from "@inngest/agent-kit";
 
 //use createFunction to define
 import { inngest } from "./client";
 import { getSandbox } from "./utils";
+import z, { object } from "zod";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -20,6 +21,92 @@ export const helloWorld = inngest.createFunction(
       system:
         "You are an expert next.js developer. You write readable and maintainable code. You write simple Next.js and React snippets.",
       model: gemini({ model: "gemini-2.0-flash" }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "Use the terminal to run commands",
+          parameters: z.object({ command: z.string() }),
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffers = { stdout: "", stderr: "" };
+
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stderr += data;
+                  },
+                });
+                return result.stdout;
+              } catch (e) {
+                console.error(
+                  `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr}`,
+                );
+                return `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr}`;
+              }
+            });
+          },
+        }),
+        createTool({
+          name: "createOrUpdateFiles",
+          description: "Create or update files in the sandbox",
+          parameters: z.object({
+            files: z.array(
+              z.object({
+                path: z.string(),
+                content: z.string(),
+              }),
+            ),
+          }),
+          handler: async ({ files }, { step, network }) => {
+            const newFiles = await step?.run(
+              "createOrUpdateFiles",
+              async () => {
+                try {
+                  const updatedFiles = network.state.data.files || {};
+                  const sandbox = await getSandbox(sandboxId);
+                  for (const file of files) {
+                    await sandbox.files.write(file.path, file.content);
+                    updatedFiles[file.path] = file.content;
+                  }
+                  return updatedFiles;
+                } catch (e) {
+                  return "Error: " + e;
+                }
+              },
+            );
+
+            if (typeof newFiles === "object") {
+              network.state.data.files = newFiles;
+            }
+          },
+        }),
+        createTool({
+          name: "readFiles",
+          description: "Read files from the sandbox",
+          parameters: z.object({
+            files: z.array(z.string()),
+          }),
+          handler: async ({ files }, { step }) => {
+            return await step?.run("readFiles", async () => {
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const contents = [];
+                for (const file of files) {
+                  const content = await sandbox.files.read(file);
+                  contents.push({ path: file, content });
+                }
+                return JSON.stringify(contents);
+              } catch (e) {
+                return "Error: " + e;
+              }
+            });
+          },
+        }),
+      ],
     });
 
     const { output } = await codeAgent.run(
