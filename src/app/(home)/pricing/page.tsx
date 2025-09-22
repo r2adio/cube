1de77344dad93e169019799534e3791a4bbe26cc
shandilyaaryan <aryanshandilya10@gmail.com>
2025-09-22 22:50:30 +0530

    Pricing Section Improved

--- src/app/(home)/pricing/page.tsx
+++ src/app/(home)/pricing/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import Image from "next/image";
 import { dark } from "@clerk/themes";
 import { PricingTable } from "@clerk/nextjs";
 
@@ -12,17 +10,8 @@ const Page = () => {
   return (
     <div className="flex flex-col max-w-3xl mx-auto w-full">
       <section className="space-y-6 pt-[16vh] 2xl:pt-48">
-        <div className="flex flex-col items-center">
-          <Image
-            src="/logo.svg"
-            alt="Cube"
-            width={50}
-            height={59}
-            className="hidden md:block"
-          />
-        </div>
-        <h1 className="text-xl md:text-3xl font-bold text-center">Pricing</h1>
-        <p className="text-muted-foreground text-center text-sm md:text-base">
+        <h1 className="text-xl md:text-5xl font-bold text-center">Pricing</h1>
+        <p className="pb-8 text-muted-foreground text-center text-sm md:text-base">
           Choose the plan that fits your needs
         </p>
       </section>
--- src/modules/home/ui/components/navbar.tsx
+++ src/modules/home/ui/components/navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
     <nav
       className={cn(
         "p-4 bg-transparent fixed top-0 left-0 right-0 z-50 transition-all duration-200 border-b border-transparent",
-        isScrolled && "bg-background border-border",
+        isScrolled && "bg-background border-border"
       )}
     >
       <div className="max-w-5xl mx-auto w-full flex justify-between items-center">
@@ -26,22 +26,27 @@ export const Navbar = () => {
           <span className="font-semibold text-lg">Cube</span>
         </Link>
 
-        <SignedOut>
-          <div className="flex gap-2">
-            <SignUpButton>
-              <Button variant="outline" size="sm">
-                Sign up
-              </Button>
-            </SignUpButton>
-            <SignInButton>
-              <Button size="sm">Sign in</Button>
-            </SignInButton>
-          </div>
-        </SignedOut>
+        <div className="flex gap-2">
+          <Button variant="outline" size="sm">
+            <Link href="/pricing">Pricing</Link>
+          </Button>
+          <SignedOut>
+            <div className="flex gap-2">
+              <SignUpButton>
+                <Button variant="outline" size="sm">
+                  Sign up
+                </Button>
+              </SignUpButton>
+              <SignInButton>
+                <Button size="sm">Sign in</Button>
+              </SignInButton>
+            </div>
+          </SignedOut>
 
-        <SignedIn>
-          <UserControl showName />
-        </SignedIn>
+          <SignedIn>
+            <UserControl showName />
+          </SignedIn>
+        </div>
       </div>
     </nav>
   );
